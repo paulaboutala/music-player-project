56b7e07f4906cb0beef745445150108010851e51
Paula Boutala <paulamaciel.3@gmail.com>
2025-05-18 21:54:17 -0300

    Components created
    Components ItemList and SingleItem created + pages

--- music-player/src/components/Header.jsx
+++ music-player/src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import musicLogo from "../assets/logo/music-logo.svg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
-      <img className="logo" src={musicLogo} alt="Logo" />
-      <a className="header__link" href="/">
+      <Link to="/">
+        <img className="logo" src={musicLogo} alt="Logo do Spotify" />
+      </Link>
+
+      <Link to="/" className="header__link">
         <h1>Music Player</h1>
-      </a>
+      </Link>
     </div>
   );
 };
